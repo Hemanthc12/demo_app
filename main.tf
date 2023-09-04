bc4606c5678a92506dcd3052c6db83d0b9f19d25
“EnterYourName” <EnterYourEmailId>
2023-09-04 09:54:04 +0530

    all

---
+++ main.tf
@@ -0,0 +1,12 @@
+# main.tf
+
+# Configure the AWS provider
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+}
+
+# Create an Amplify App
+resource "aws_amplify_app" "my_amplify_app" {
+  name      = "my-amplify-app"  # Replace with your app name
+  repository = "https://github.com/your-username/your-repo.git" # Replace with your Git repository URL
+}
