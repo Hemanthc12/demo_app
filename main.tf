58b9c7c81c3bbbd843074cde846a33df8aa55972
“EnterYourName” <EnterYourEmailId>
2023-09-04 10:07:34 +0530

    all

--- main.tf
+++ main.tf
@@ -9,4 +9,5 @@ provider "aws" {
 resource "aws_amplify_app" "my_amplify_app" {
   name      = "my-amplify-app"  # Replace with your app name
   repository = "https://github.com/your-username/your-repo.git" # Replace with your Git repository URL
+  access_token = "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"
 }
